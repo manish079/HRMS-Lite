d0757c04bfe257f26c706a1206e4803219349d38
manish079 <manishprajapat492@gmail.com>
2026-01-29 11:18:20 +0530

    python package installed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+// const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://hrms-lite-mthb.onrender.com/api';
 
 // Create axios instance with default config
 const api = axios.create({
