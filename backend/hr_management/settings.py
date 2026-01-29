dff1244890a65ed353ced54d18478861b877d4bd
manish079 <manishprajapat492@gmail.com>
2026-01-29 12:14:21 +0530

    cors error

--- backend/hr_management/settings.py
+++ backend/hr_management/settings.py
@@ -119,7 +119,7 @@ REST_FRAMEWORK = {
 # CORS settings
 CORS_ALLOWED_ORIGINS = config(
     'CORS_ALLOWED_ORIGINS',
-    default='http://localhost:3000,http://localhost:5173'
+    default='http://localhost:3000,http://localhost:5173, https://hrmanagmentfrontend.netlify.app/'
 ).split(',')
 
 CORS_ALLOW_CREDENTIALS = True
