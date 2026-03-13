11b2efa08e5f1d5b91a207e022e5664f5548a39a
manish079 <manishprajapat492@gmail.com>
2026-03-13 15:05:07 +0530

    'update'

--- backend/hr_management/settings.py
+++ backend/hr_management/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
     }
 }
 
-# Password validation
+# 
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
